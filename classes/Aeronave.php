959a534e68abf35fdf517a845834d4770589433d
itzvini <106489329+itzvini@users.noreply.github.com>
2023-04-25 16:36:16 -0300

    Add files via upload

---
+++ classes/Aeronave.php
@@ -0,0 +1,159 @@
+<?php
+
+include_once("CompanhiaAerea.php");
+
+class Aeronave extends persist{
+
+protected string $fabricante;
+protected string $modelo;
+protected float $carga;
+protected int $passageiros;
+protected string $registro;
+protected CompanhiaAerea $CompanhiaAerea_;
+
+
+public function __construct($fabricante_f,$modelo_f,$carga_f,$passageiros_f,$registro_f,$companhiaAerea_f){
+    $this->set_fabricante($fabricante_f);
+    $this->set_modelo($modelo_f);
+    $this->set_carga($carga_f);
+    $this->set_passageiros($passageiros_f);
+    $this->set_registro($registro_f);
+    $this->set_companhia($companhiaAerea_f);
+    $companhiaAerea_f->set_avioes($this);
+}
+
+public function validar_registro($registro_f){
+// Composto pelo prefixo, que contém duas letras:
+// Um hífen
+// Seguido de três letras
+// Ex.: PR-GUO)
+// No Brasil, somente são permitidos para voos comerciais os
+// prefixos PT, PR, PP, PS, que devem ser validados.
+
+    $prefixo = substr($registro_f,0,2);
+    $hifen = substr($registro_f,2,1);
+    $sufixo = substr($registro_f,3,3);
+    if (ctype_alpha($prefixo)==true && $hifen=='-' && ctype_alpha($sufixo)==true && $prefixo=='PT' || $prefixo=='PR' || $prefixo=='PP' || $prefixo=='PS'){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+static public function getFilename() {
+    return get_called_class()::$local_filename;
+}
+public function get_fabricante(){
+    return $this->fabricante;
+}
+
+public function get_modelo(){
+    return $this->modelo;
+}
+
+public function get_carga(){
+    return $this->carga;
+}
+
+public function get_passageiro(){
+    return $this->passageiros;
+}
+
+public function get_registro(){
+    return $this->registro;
+}
+
+public function get_companhia_aerea(){
+    return $this->CompanhiaAerea_;
+}
+
+public function set_fabricante($fabricante_f){
+    try{
+        if (ctype_alpha($fabricante_f)==false){
+            throw new Exception("Fabricante inválido");
+        }
+        else{
+            $this->fabricante = $fabricante_f;
+        }
+    }
+    catch(Exception $e){
+        echo $e->getMessage();
+}}
+
+public function set_modelo($modelo_f){
+    //Composto por um prefixo de uma letra
+    //Um hifen
+    //Seguido de um número de 3 dígitos
+    //Modelo (Ex.: A-320
+    try{
+        $prefixo = substr($modelo_f,0,1);
+        $hifen = substr($modelo_f,1,1);
+        $sufixo = substr($modelo_f,2,3);
+        if (ctype_alpha($prefixo)==false || $hifen!='-' || ctype_digit($sufixo)==false){
+            throw new Exception("Modelo inválido");
+        }
+        else{
+            $this->modelo = $modelo_f;
+        }
+    }
+    catch(Exception $e){
+        echo $e->getMessage();
+    }
+}
+
+public function set_carga($carga_f){
+    try{
+        if (is_numeric($carga_f)==false){
+            throw new Exception("Carga inválida");
+        }
+        else{
+            $this->carga = $carga_f;
+        }
+    }
+    catch(Exception $e){
+        echo $e->getMessage();
+    }
+}
+
+public function set_passageiros($passageiro_f){
+    try{
+        if (is_numeric($passageiro_f)==false){
+            throw new Exception("Passageiro inválido");
+        }
+        else{
+            $this->passageiros = $passageiro_f;
+        }
+    }
+    catch(Exception $e){
+        echo $e->getMessage();
+}}
+
+public function set_registro($registro_f){
+    try {
+        if ($this->validar_registro($registro_f)==false){
+            throw new Exception("Registro inválido");
+        }
+        else{
+            $this->registro = $registro_f;
+        }
+    }
+    catch(Exception $e){
+        echo $e->getMessage();
+    }
+}
+
+public function set_companhia(CompanhiaAerea $companhia_aerea_f){
+    try {
+        if ($companhia_aerea_f instanceof CompanhiaAerea==false){
+            throw new Exception("Companhia Aérea inválida");
+        }
+        else{
+            $this->CompanhiaAerea_ = $companhia_aerea_f;
+        }
+    }
+    catch(Exception $e){
+        echo $e->getMessage();
+    }
+}
+};
+?>
---
+++ classes/Aeroporto.php
@@ -0,0 +1,93 @@
+<?php
+
+class Aeroporto extends persist{
+protected string $sigla;
+protected string $cidade;
+protected string $estado;
+protected string $nome;
+
+public function __construct(string $sigla_f,string $cidade_f,string $estado_f,string $nome_f){
+    $this->set_sigla_aero($sigla_f);
+    $this->set_cidade($cidade_f);
+    $this->set_estado($estado_f);
+    $this->set_nome($nome_f);
+}
+
+static public function getFilename() {
+  return get_called_class()::$local_filename;
+}
+
+public function validar_sigla_aero(string $sigla_s){
+//A sigla deve ser formada por três letras
+    if (ctype_alpha($sigla_s) && strlen($sigla_s) == 3){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+public function get_sigla_aero(){
+  return $this->sigla;
+}
+
+public function get_cidade(){
+  return $this->cidade;
+}
+
+public function get_estado(){
+  return $this->estado;
+}
+
+public function get_nome_aero(){
+  return $this->nome;
+}
+
+public function set_sigla_aero(string $sigla_f){
+  try{
+    if ($this->validar_sigla_aero($sigla_f)){
+      $this->sigla = $sigla_f;
+    }else{
+      throw new Exception("Sigla inválida");
+    }
+  }catch(Exception $e){
+    echo $e->getMessage();
+  }
+}
+
+public function set_cidade(string $cidade_f){
+ try{
+    if (ctype_alpha($cidade_f)){
+      $this->cidade = $cidade_f;
+    }else{
+      throw new Exception("Cidade inválida");
+    }
+  }catch(Exception $e){
+    echo $e->getMessage();
+  }
+}
+
+public function set_estado(string $estado_f){
+  try{
+    if (ctype_alpha($estado_f)){
+      $this->estado = $estado_f;
+    }else{
+      throw new Exception("Estado inválido");
+    }
+  }catch(Exception $e){
+    echo $e->getMessage();
+  }
+}
+
+public function set_nome(string $nome_f){
+  try{
+    if (is_string($nome_f)){
+      $this->nome = $nome_f;
+    }else{
+      throw new Exception("Nome inválido");
+    }
+}
+  catch(Exception $e){
+    echo $e->getMessage();
+  }
+}
+}
