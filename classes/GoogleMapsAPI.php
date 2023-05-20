0ae76dab34a7df17f8ca12720d729aa0d26f8e16
itzvini <viniciusmc.2109@gmail.com>
2023-05-20 19:18:18 -0300

    rotas e integração com API da Google

---
+++ classes/GoogleMapsAPI.php
@@ -0,0 +1,80 @@
+<?php
+
+require_once 'requests.php';
+class GoogleMapsAPI
+{
+    private $apiKey = "AIzaSyDg7gWEqeFaPMQqMmCeWDpFCF7-WiBaW-w";
+    private $ch;
+    
+    public function __construct()
+    {
+        $this->ch = curl_init();
+        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
+    }
+    
+    public function geocode($address)
+    {
+        $url = 'https://maps.googleapis.com/maps/api/geocode/json';
+        
+        $queryParams = [
+            'address' => $address,
+            'key' => $this->apiKey
+        ];
+        
+        return $this->get($url, $queryParams);
+    }
+    
+    public function directions($origin, $destination, $waypoints = [], $optimize = false)
+{
+    $url = 'https://maps.googleapis.com/maps/api/directions/json';
+    
+    $queryParams = [
+        'origin' => $origin,
+        'destination' => $destination,
+        'key' => $this->apiKey
+    ];
+
+    // Adiciona waypoints se existirem
+    if (!empty($waypoints)) {
+        $waypointsParam = implode('|', $waypoints);
+        $queryParams['waypoints'] = $waypointsParam;
+    }
+    
+    // Define a otimização de rota se necessário
+    if ($optimize && !empty($waypoints)) {
+        $queryParams['optimize_waypoints'] = 'true';
+    }
+    
+    return $this->get($url, $queryParams);
+}
+    
+    private function get($url, $queryParams = [])
+    {
+        if (!empty($queryParams)) {
+            $url .= '?' . http_build_query($queryParams);
+        }
+        
+        curl_setopt($this->ch, CURLOPT_URL, $url);
+        curl_setopt($this->ch, CURLOPT_HTTPGET, true);
+        
+        return $this->execute();
+    }
+    
+    private function execute()
+    {
+        $response = curl_exec($this->ch);
+        
+        if (curl_errno($this->ch)) {
+            echo 'Erro na requisição: ' . curl_error($this->ch);
+            // Você pode adicionar lógica para lidar com o erro aqui
+        }
+        
+        return $response;
+    }
+    
+    public function close()
+    {
+        curl_close($this->ch);
+    }
+
+}
